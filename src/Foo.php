d995f3ab641e2b9f9756931732d316d0782d05f3
Eduard Bess <eduard.bess@gmail.com>
2019-09-14 12:37:45 +0200

    # test

---
+++ src/Foo.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Ebess\CiTest;
+
+class Foo
+{
+    public function make(): int
+    {
+        return 5;
+    }
+}
---
+++ tests/FooTest.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Ebess\Tests;
+
+use Ebess\CiTest\Foo;
+use PHPUnit\Framework\TestCase;
+
+class FooTest extends TestCase
+{
+    public function testFooMake()
+    {
+        $fut = new Foo;
+
+        $this->assertEquals(5, $fut->make(), 'Make has to return 5.');
+    }
+
+    public function testFooFail()
+    {
+        $fut = new Foo;
+
+        $this->assertNull($fut->make(), 'Make has to fail right here.');
+    }
+}
